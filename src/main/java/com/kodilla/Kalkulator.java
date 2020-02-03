932b944f78fee30a8942fa44589241822b7334ca
fordon-86@o2.pl <fordon-86@o2.pl>
2020-02-03 19:47:48 +0100

    Calculator

---
+++ src/main/java/com/kodilla/Kalkulator.java
@@ -0,0 +1,20 @@
+package com.kodilla;
+
+class Calculator {
+
+    public void minus(){
+        System.out.println("It works!");
+    }
+
+    public void plus() {
+
+    }
+
+    public static void main(String args[]){
+        Calculator calculator = new Calculator();
+        calculator.minus();
+
+
+    }
+
+}
